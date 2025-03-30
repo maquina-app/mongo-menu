9802091c8a170a35ffaf744f58034bff53e58c7d
Mario Alberto Chávez <mario.chavez@gmail.com>
2025-03-30 12:46:40 -0600

    Initial Commit

---
+++ MongoMenu/MongoMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MongoMenuApp.swift
+//  MongoMenu
+//
+//  Created by Mario Chavez on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MongoMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
